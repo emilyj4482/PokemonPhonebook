e88a724a11cfb6be0960e3d14cdd597ad1773cff
emilyj4482 <emilyj4482@gmail.com>
2024-12-08 04:32:48 +0900

    feat: UINavigationController 적용, navigation bar 추가

--- PokemonPhonebook/MainListViewController.swift
+++ PokemonPhonebook/MainListViewController.swift
@@ -11,6 +11,22 @@ class MainListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigationBar()
+    }
+    
+    private func setNavigationBar() {
+        navigationItem.title = "친구 목록"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "추가", style: .plain, target: self, action: #selector(addButtonTapped))
     }
 
+}
+
+extension MainListViewController {
+    @objc func addButtonTapped() {
+        navigationController?.pushViewController(PhoneBookViewController(), animated: true)
+    }
+}
+
+#Preview {
+    UINavigationController(rootViewController: MainListViewController())
 }
---
+++ PokemonPhonebook/PhoneBookViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PhoneBookViewController.swift
+//  PokemonPhonebook
+//
+//  Created by EMILY on 08/12/2024.
+//
+
+import UIKit
+
+class PhoneBookViewController: UIViewController {
+    
+}
