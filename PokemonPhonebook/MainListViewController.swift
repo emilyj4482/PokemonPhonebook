6f79a10aef2ff523d40f6ad133929bb32005216f
emilyj4482 <emilyj4482@gmail.com>
2024-12-08 04:12:19 +0900

    chore: UIKit code base 프로젝트 생성

---
+++ PokemonPhonebook/MainListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  PokemonPhonebook
+//
+//  Created by EMILY on 08/12/2024.
+//
+
+import UIKit
+
+class MainListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
