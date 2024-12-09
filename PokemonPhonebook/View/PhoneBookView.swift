afd12b68e211059a75ac37330b377e8cb7c66644
emilyj4482 <emilyj4482@gmail.com>
2024-12-09 16:39:00 +0900

    feat: PhoneBookView ui 구성

--- PokemonPhonebook/View/PhoneBookView.swift
+++ PokemonPhonebook/View/PhoneBookView.swift
@@ -8,9 +8,42 @@
 import UIKit
 
 class PhoneBookView: UIView {
+    
+    private lazy var randomImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    private lazy var fetchButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("랜덤 이미지 생성", for: .normal)
+        button.setTitleColor(.darkGray, for: .normal)
+        button.setTitleColor(.lightGray, for: .highlighted)
+        button.addTarget(self, action: #selector(fetchButtonTapped), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private lazy var nameTextView: UITextView = {
+        let textView = UITextView()
+        
+        return textView
+    }()
+    
+    private lazy var numberTextView: UITextView = {
+        let textView = UITextView()
+        
+        return textView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         layout()
+        configure(PhoneBook.dummy)
     }
     
     required init?(coder: NSCoder) {
@@ -18,6 +51,47 @@ class PhoneBookView: UIView {
     }
     
     private func layout() {
+        addSubViews([randomImageView, fetchButton, nameTextView, numberTextView])
         
+        let offset: CGFloat = 16
+        
+        randomImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(offset)
+            $0.leading.equalToSuperview().offset(120)
+            $0.trailing.equalToSuperview().offset(-120)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(randomImageView.snp.width)
+        }
+        
+        fetchButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(randomImageView.snp.bottom).offset(offset)
+        }
+        
+        nameTextView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(fetchButton.snp.bottom).offset(-32)
+            $0.leading.equalToSuperview().offset(offset)
+            $0.trailing.equalToSuperview().offset(-offset)
+        }
+        
+        numberTextView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(nameTextView.snp.bottom).offset(-offset)
+            $0.leading.equalToSuperview().offset(offset)
+            $0.trailing.equalToSuperview().offset(-offset)
+        }
+    }
+    
+    private func configure(_ phoneBook: PhoneBook) {
+        randomImageView.image = UIImage(named: phoneBook.imageURL)
+        nameTextView.text = phoneBook.name
+        numberTextView.text = phoneBook.phoneNumber
+    }
+}
+
+extension PhoneBookView {
+    @objc func fetchButtonTapped() {
+        print("tapped")
     }
 }
